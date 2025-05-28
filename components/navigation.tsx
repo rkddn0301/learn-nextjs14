b9a3d9e08b3e4019ebd9fb7d3a614cfa1830b59b
rkddn0301 <rkddn0301@naver.com>
2025-05-28 19:23:05 +0900

    2.6 Recap

--- components/navigation.tsx
+++ components/navigation.tsx
@@ -1,9 +1,12 @@
-"use client"; // 클라이언트 컴포넌트 구분
+"use client"; // 기본은 서버 컴포넌트지만 클라이언트 컴포넌트로도 사용한다는 의미. hook 사용 시 반드시 넣어줘야함.
+
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { useState } from "react";
 
 export default function Navigation() {
   const path = usePathname();
+  const [count, setCount] = useState(0);
   return (
     <nav>
       <ul>
@@ -13,6 +16,9 @@ export default function Navigation() {
         <li>
           <Link href="/about-us">About Us</Link>{" "}
           {path === "/about-us" ? "📌" : ""}
+        </li>
+        <li>
+          <button onClick={() => setCount((c) => c + 1)}>{count}</button>
         </li>
       </ul>
     </nav>
